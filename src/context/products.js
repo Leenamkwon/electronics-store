6079adddcf71aff3d197c657eea22a292382c2b4
Leenamkwon <namkwon12@gmail.com>
2020-09-26 09:49:49 +0900

    context setup

--- src/context/products.js
+++ src/context/products.js
@@ -1,1 +1,19 @@
 // products context
+import React, { createContext, useState } from 'react';
+
+const ProductContext = createContext();
+
+// Provider, Consumer
+const ProductProvider = ({ children }) => {
+  const [loading, setLoading] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [featured, setFeatured] = useState([]);
+
+  return (
+    <ProductContext.Provider value={{ loading, products, featured }}>
+      {children}
+    </ProductContext.Provider>
+  );
+};
+
+export { ProductProvider, ProductContext };
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useContext } from 'react';
+import { ProductContext } from '../context/products';
 
 export default function Products() {
+  const { products, loading, featured } = useContext(ProductContext);
+
   return <h1>hello from products page</h1>;
 }
