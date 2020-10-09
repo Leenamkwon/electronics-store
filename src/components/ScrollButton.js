05458114375b32093aa5078d3b518e550036ce00
Leenamkwon <namkwon12@gmail.com>
2020-10-10 03:32:26 +0900

    filter set up

--- src/components/ScrollButton.js
+++ src/components/ScrollButton.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext } from 'react';
 import { FaAngleDoubleUp } from 'react-icons/fa';
 import { UserContext } from '../context/user';
 
